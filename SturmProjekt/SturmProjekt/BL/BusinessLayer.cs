5cd1c3c34bf4ecf200440139e57e9f62d7913a47
unknown <Sebastian@SEBASTIAN-DTW10.slowak.cc>
2017-12-14 14:29:09 +0100

    fixed save as pdf, added sammelpdf (not working correctly), missing UI fix, BL refactor, save to excel

--- SturmProjekt/SturmProjekt/BL/BusinessLayer.cs
+++ SturmProjekt/SturmProjekt/BL/BusinessLayer.cs
@@ -8,10 +8,13 @@ using System.Linq;
 using System.Text;
 using System.Windows.Media.Imaging;
 using Newtonsoft.Json;
+using PdfSharp.Drawing;
+using PdfSharp.Pdf;
 using Spire.Pdf;
 using Spire.Pdf.Graphics;
 using SturmProjekt.Models;
 using Brushes = System.Drawing.Brushes;
+using PdfDocument = Spire.Pdf.PdfDocument;
 using Pen = System.Drawing.Pen;
 
 namespace SturmProjekt.BL
@@ -217,18 +220,9 @@ namespace SturmProjekt.BL
             }
             List<Bitmap> cutOutBitmaps = _rechnungsLogic.GetCutOutBitmaps(bitmaps, profile.Pages);
             List<string> directories = _rechnungsLogic.GetCategoryDirectories(DataPath);
-            List<Bitmap> logoList = new List<Bitmap>();
-            foreach (var directory in directories)
-            {
-                if (!directory.EndsWith("_Undefined"))
-                {
-                    var logo = directory + "\\logo.jpg";
-                    if (File.Exists(logo))
-                    {
-                        logoList.Add(ConvertImageToBitmap(logo));
-                    }
-                }           
-            }
+
+            List<Bitmap> logoList = GetLogos();
+           
             int logoindex = 0;
             int chosenlogo = -1;
             foreach (var logoBitmap in logoList)
@@ -260,6 +254,25 @@ namespace SturmProjekt.BL
             SaveToCsv(values);
             DisposeBitMaps(rechnung);
         }
+
+        public List<Bitmap> GetLogos()
+        {
+            List<Bitmap> logoList = new List<Bitmap>();
+            List<string> directories = _rechnungsLogic.GetCategoryDirectories(DataPath);
+            foreach (var directory in directories)
+            {
+                if (!directory.EndsWith("_Undefined"))
+                {
+                    var logo = directory + "\\logo.jpg";
+                    if (File.Exists(logo))
+                    {
+                        logoList.Add(ConvertImageToBitmap(logo));
+                    }
+                }
+            }
+            return logoList;
+        }
+
 
         public void DisposeBitMaps(RechnungsModel rechnung)
         {
@@ -317,21 +330,19 @@ namespace SturmProjekt.BL
 
         public void SaveRechnungAsPDF(RechnungsModel rechnung, string category)
         {
-            PdfDocument doc = new PdfDocument();
+            var doc = new PdfSharp.Pdf.PdfDocument();
+
+            int index = 0;
             foreach (var rechnungPage in rechnung.Pages)
             {
-                PdfPageBase page = doc.Pages.Add();
-                PdfTemplate template = new PdfTemplate(PdfPageSize.A4);
-                PdfImage image = PdfImage.FromImage(rechnungPage.Page);
+                doc.Pages.Add(new PdfPage());
+                XGraphics xgr = XGraphics.FromPdfPage(doc.Pages[index]);
+                XImage img = XImage.FromBitmapSource(rechnungPage.PageImage);
 
-                template.Graphics.DrawImage(image, 100, 100);
-
-                  page.Canvas.DrawImage(image, 0, 0, page.Canvas.ClientSize.Width, page.Canvas.ClientSize.Height);
-               // page.BackgroundImage = rechnungPage.Page;
-               // page.BackgroudOpacity = 1;
-
-              
+                xgr.DrawImage(img, 0, 0, 592, 840);
+                index++;
             }
+
             var sb = new StringBuilder();
             sb.Append(category);
             sb.Append("\\");
@@ -339,9 +350,10 @@ namespace SturmProjekt.BL
             sb.Append(".pdf");
             var savepath = sb.ToString();
 
-            doc.SaveToFile(savepath);
+            doc.Save(savepath);
             doc.Close();
         }
+
 
         public void SortDirectoryFiles(List<FileModel> fileModels, ProfileModel selectedProfile)
         {
@@ -388,15 +400,188 @@ namespace SturmProjekt.BL
         {
             var pdf = GetPdfDocument(filename);
             var rechnung = new RechnungsModel();
+            int rechcount = 0;
             var firstpage = pdf.SaveAsImage(0);
             var firstpagebmp = new Bitmap(firstpage);
-            
+            firstpagebmp = ResizeBitmap(firstpagebmp);
+            var bitmaplist = new List<Bitmap>();
+            bitmaplist.Add(firstpagebmp);
+            List<Bitmap> cutOutBitmaps = _rechnungsLogic.GetCutOutBitmaps(bitmaplist, selectedprofile.Pages);
+            List<Bitmap> logoList = GetLogos();
+            int chosenlogo = -1;
+            int logoindex = 0;
+            List<string> directories = _rechnungsLogic.GetCategoryDirectories(DataPath);
+            string category;
+            foreach (var logoBitmap in logoList)
+            {
+
+                var difference = _rechnungsLogic.Test(logoBitmap, cutOutBitmaps.First());
+                if (difference <= 20.0f)
+                {
+                    chosenlogo = logoindex;
+                    break;
+                }
+
+                logoindex++;
+            }
+            if (chosenlogo.Equals(-1))
+            {
+                return;
+            }
+            else
+            {
+                var rech = GetFileNameFromFilePath(filename);
+                rech = rech.Split('.')[0];
+                rechnung.Name = rech + "_" + rechcount;
+                var picture = new PictureModel();
+                picture.FileName = filename;
+                picture.Page = firstpagebmp;
+                picture.PageImage = BitmapToImageSource(firstpagebmp);
+                rechnung.Pages = new List<PictureModel>();
+                rechnung.Pages.Add(picture);
+                rechnung.PageCount = rechnung.Pages.Count;
+                category = GetChosenCategory(directories, logoindex);
+            }
+            for (var index = 1; index < pdf.Pages.Count; index++)
+            {
+                var page = pdf.SaveAsImage(index);
+                var pagebmp = new Bitmap(page);
+                pagebmp = ResizeBitmap(pagebmp);
+                pagebmp.Save(@"I:\vgames\img" + index + ".jpg", ImageFormat.Jpeg);
+                var bmplist = new List<Bitmap>();            
+                bmplist.Add(pagebmp);
+
+                int chosen = -1;
+                int logoindx = 0;
+                List<Bitmap> cutout = _rechnungsLogic.GetCutOutBitmaps(bmplist, selectedprofile.Pages);
+                foreach (var logoBitmap in logoList)
+                {
+
+                    var difference = _rechnungsLogic.Test(logoBitmap, cutout.First());
+                    if (difference <= 20.0f)
+                    {
+                        chosen = logoindx;
+                        break;
+                    }
+
+                    logoindx++;
+                }
+                if (chosen.Equals(-1))
+                {
+                    var pic = new PictureModel
+                    {
+                        FileName = filename,
+                        Page = pagebmp,
+                        PageImage = BitmapToImageSource(pagebmp)
+                    };
+
+                    rechnung.Pages.Add(pic);
+                    rechnung.PageCount++;
+                }
+                else
+                {
+                    SaveRechnungAsPDF(rechnung, category);
+                    DisposeBitMaps(rechnung);
+                    rechcount++;
+                    rechnung = new RechnungsModel();
+
+                    var pic = new PictureModel
+                    {
+                        FileName = filename,
+                        Page = pagebmp,
+                        PageImage = BitmapToImageSource(pagebmp)
+                    };
+                    rechnung.Pages = new List<PictureModel>();
+                    rechnung.Pages.Add(pic);
+                    rechnung.PageCount = rechnung.Pages.Count;
+                    var rech = GetFileNameFromFilePath(filename);
+                    rech = rech.Split('.')[0];
+                    rechnung.Name = rech + "_" + rechcount;
+                }
+
+            }
+
+
             /* TODO: erste page nachschauen um welchen Rechnungstyp es sich handelt.
              * TODO: dann für jede weitere page nachschauen ob es sich um den selben Rechnungstyp handelt, 
              * TODO: wenn ja, dann Rechnung fertigstellen und cutoutbitmaps ausführen für CSV-Daten ...
-             */ 
+             */
 
         }
+
+     /*   public PictureModel CreateFirstPage(Bitmap bitmap, string filename)
+        {
+            var first = new PictureModel();
+            first.Page = ResizeBitmap(bitmap);
+            first.PageImage = BitmapToImageSource(ResizeBitmap(bitmap));
+            first.FileName = filename;
+
+            return first;
+        }
+
+        public void WriteToExcelFile()
+        {
+            Microsoft.Office.Interop.Excel.Application oXL;
+            Microsoft.Office.Interop.Excel._Workbook oWB;
+            Microsoft.Office.Interop.Excel._Worksheet oSheet;
+            Microsoft.Office.Interop.Excel.Range oRng;
+            object misvalue = System.Reflection.Missing.Value;
+            try
+            {
+                //Start Excel and get Application object.
+                oXL = new Microsoft.Office.Interop.Excel.Application();
+                oXL.Visible = true;
+
+                //Get a new workbook.
+                oWB = (Microsoft.Office.Interop.Excel._Workbook) (oXL.Workbooks.Add(""));
+                oSheet = (Microsoft.Office.Interop.Excel._Worksheet) oWB.ActiveSheet;
+
+                //Add table headers going cell by cell.
+                oSheet.Cells[1, 1] = "First Name";
+                oSheet.Cells[1, 2] = "Last Name";
+                oSheet.Cells[1, 3] = "Full Name";
+                oSheet.Cells[1, 4] = "Salary";
+
+                //Format A1:D1 as bold, vertical alignment = center.
+                oSheet.get_Range("A1", "D1").Font.Bold = true;
+                oSheet.get_Range("A1", "D1").VerticalAlignment =
+                    Microsoft.Office.Interop.Excel.XlVAlign.xlVAlignCenter;
+
+                // Create an array to multiple values at once.
+                string[,] saNames = new string[5, 2];
+
+                saNames[0, 0] = "John";
+                saNames[0, 1] = "Smith";
+                saNames[1, 0] = "Tom";
+
+                saNames[4, 1] = "Johnson";
+
+                //Fill A2:B6 with an array of values (First and Last Names).
+                oSheet.get_Range("A2", "B6").Value2 = saNames;
+
+                //Fill C2:C6 with a relative formula (=A2 & " " & B2).
+                oRng = oSheet.get_Range("C2", "C6");
+                oRng.Formula = "=A2 & \" \" & B2";
+
+                //Fill D2:D6 with a formula(=RAND()*100000) and apply format.
+                oRng = oSheet.get_Range("D2", "D6");
+                oRng.Formula = "=RAND()*100000";
+                oRng.NumberFormat = "$0.00";
+
+                //AutoFit columns A:D.
+                oRng = oSheet.get_Range("A1", "D1");
+                oRng.EntireColumn.AutoFit();
+
+                oXL.Visible = false;
+                oXL.UserControl = false;
+                oWB.SaveAs("c:\\test\\test505.xls", Microsoft.Office.Interop.Excel.XlFileFormat.xlWorkbookDefault,
+                    Type.Missing, Type.Missing,
+                    false, false, Microsoft.Office.Interop.Excel.XlSaveAsAccessMode.xlNoChange,
+                    Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing);
+
+                oWB.Close();
+            }
+        } */
 
     }
 
